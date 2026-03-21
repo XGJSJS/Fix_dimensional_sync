d181fef6ad3efb253f36e67ce1a0e28a73a6df98
XGJSJS <XZTXZTXZT666@163.com>
2026-03-21 08:48:08 +0800

    1.0.1

--- src/main/java/net/xgjs/fds/mixin/ServerPlayerMixin.java
+++ src/main/java/net/xgjs/fds/mixin/ServerPlayerMixin.java
@@ -5,15 +5,12 @@ import net.minecraft.network.protocol.game.ClientboundUpdateMobEffectPacket;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.RelativeMovement;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.util.Set;
 
 @Mixin(ServerPlayer.class)
 public class ServerPlayerMixin {
@@ -36,8 +33,8 @@ public class ServerPlayerMixin {
 		syncInfo((ServerPlayer) cir.getReturnValue());
 	}
 
-	@Inject(method = "teleportTo(Lnet/minecraft/server/level/ServerLevel;DDDLjava/util/Set;FF)Z", at = @At("TAIL"))
-	private void afterTeleportTo(ServerLevel serverLevel, double d, double e, double f, Set<RelativeMovement> set, float g, float h, CallbackInfoReturnable<Boolean> cir) {
+	@Inject(method = "teleportTo(Lnet/minecraft/server/level/ServerLevel;DDDFF)V", at = @At("TAIL"))
+	private void afterTeleportTo(ServerLevel serverLevel, double d, double e, double f, float g, float h, CallbackInfo ci) {
 		syncInfo((ServerPlayer) (Object) this);
 	}
 
