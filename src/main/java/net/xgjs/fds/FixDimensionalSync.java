577846fbed6e07c1869fc7a2b63730bf88fb5608
XGJSJS <XZTXZTXZT666@163.com>
2026-07-16 20:25:00 +0800

    3.0.0 Support 1.16-1.17.1

--- src/main/java/net/xgjs/fds/FixDimensionalSync.java
+++ src/main/java/net/xgjs/fds/FixDimensionalSync.java
@@ -1,13 +1,12 @@
 package net.xgjs.fds;
 
 import net.fabricmc.api.ModInitializer;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class FixDimensionalSync implements ModInitializer {
 	public static final String MOD_ID = "fix_dimensional_sync";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
